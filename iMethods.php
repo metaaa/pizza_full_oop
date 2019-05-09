084dd9865f3046da85563216860257c349f9d0a1
root <root@debian.metaaa.me>
2019-05-09 14:38:26 +0200

    interface changed

--- iMethods.php
+++ iMethods.php
@@ -3,8 +3,8 @@
 interface iMethods{
     public function validate();
     public function isNewRecord();
-    public function add(...$params);
-    public function modify(...$params);
-    public function remove($id);
+    public function add();
+    public function modify();
+    public function remove();
     public function save();
 }
