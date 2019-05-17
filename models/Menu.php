9ae59f85413db74103558b4e6534b04a11785264
root <root@debian.metaaa.me>
2019-05-17 14:40:27 +0200

    menu with auto width added

--- models/Menu.php
+++ models/Menu.php
@@ -2,12 +2,19 @@
 
 class Menu extends Dbconfig
 {
+    public static function countRows(){
+        $countQuery = "SELECT COUNT(mId) FROM menu";
+        $countResult = Dbconfig::getInstance()->getConnection()->query($countQuery)->fetch_array();
+        //print_r($countResult);
+        return (100 / $countResult[0]);
+    }
+
     public static function getAll()
     {
-                $menuQuery = "SELECT * FROM menu";
+        $menuQuery = "SELECT * FROM menu";
         $menuQueryResult = Dbconfig::getInstance()->getConnection()->query($menuQuery);
         while ($row = $menuQueryResult->fetch_assoc()){
-            echo '<li style="width: 100px"><a href="index.php?page=' . $row['mLink'] . '">' . $row['mName'] . '</a>';
+            echo '<li style="width: ' . self::countRows() . '%"><a href="index.php?page=' . $row['mLink'] . '">' . $row['mName'] . '</a>';
         }
     }
 }
