666e21ac2d952c86089a1ca65f66ad96855bfc9b
metaaa <BacktoGH2013>
2019-05-22 21:41:00 +0200

    login added

---
+++ content-files/login.php
@@ -0,0 +1,10 @@
+<?php
+
+
+?>
+
+<form action="/pizza_oop/controllers/loginController.php" class="register-form" method="POST">
+    <input name="username" type="input">
+    <input name="password" type="password">
+    <button type="submit">Submit</button>
+</form>
